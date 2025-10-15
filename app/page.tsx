fb1cc42f36bf364aff20c1014f8dc61014cdf285
Karen <techshift86@gmail.com>
2025-10-15 19:35:46 +0400

    fjdk

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-'use client' // remove this
+
 
 import React from "react";
 import Template from "./Template";
 
-// export default function Home() {
-//   return <Template/>
-// }
-
-
 export default function Home() {
-  if (typeof window !== "undefined") {
-    window.location.href = "https://sites.google.com/view/ashevillehandyman/home";
-  }
-  return null;
+  return <Template/>
 }
+
+
